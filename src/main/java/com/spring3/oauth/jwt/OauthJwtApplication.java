ef54d999d4178a375ac20d1121776b66554da427
Per Olsen <persteen22@gmail.com>
2025-02-20 13:38:48 +0100

    Added code for deploy to Tomcat

--- src/main/java/com/spring3/oauth/jwt/OauthJwtApplication.java
+++ src/main/java/com/spring3/oauth/jwt/OauthJwtApplication.java
@@ -2,13 +2,20 @@ package com.spring3.oauth.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class OauthJwtApplication {
+public class OauthJwtApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthJwtApplication.class, args);
     }
 
-	
+	// Ready for builing a war file used for Tomcat at Azure
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OauthJwtApplication.class);
+	}
 }
